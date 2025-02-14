41824c957872f2657cdfdeeddffba5dcc455aa52
CROMABLUE <jsmoura.dev@gmail.com>
2025-02-14 13:05:38 -0400

    :bug: fix: reincluding excluded variable

--- ReservaUFAM/models.py
+++ ReservaUFAM/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     siape = models.CharField(max_length=7, unique=True)
     CPF = models.CharField(max_length=11, unique=True)
     name = models.CharField(max_length=255)
