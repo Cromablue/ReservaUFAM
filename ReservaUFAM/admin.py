e93a2069a8217ba2fed7336c60b8316a25cc36e7
evelimbacury <evelimbacury@gmail.com>
2025-02-14 09:35:53 -0400

    :sparkles: feat(admin): register models in Django admin

--- ReservaUFAM/admin.py
+++ ReservaUFAM/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserProfile, AdminProfile, Reserva
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(AdminProfile)
+admin.site.register(Reserva)
