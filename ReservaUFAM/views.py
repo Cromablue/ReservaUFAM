2173b555947437ba1165da425b93f878b60e078d
CROMABLUE <jsmoura.dev@gmail.com>
2025-02-14 13:52:07 -0400

    :sparkles: feat: add new view, register_user

--- ReservaUFAM/views.py
+++ ReservaUFAM/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from  .forms import UserForm 
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.password = make_password(form.cleaned_data["password"])
+            user.save()
+            return redirect('login')
+    else:
+        form = UserForm()
+    return render(request, 'register.html', {'form': form})
