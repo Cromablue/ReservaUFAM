083f20da063c2b1349c1b6c54589ab9194a4f91c
CROMABLUE <jsmoura.dev@gmail.com>
2025-02-14 15:01:47 -0400

    :bug: fix: fix the name of the fields

--- ReservaUFAM/forms.py
+++ ReservaUFAM/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class UserForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['siape', 'CPF', 'name', 'email', 'cellphone', 'password']
+        fields = ['siape', 'cpf', 'name', 'email', 'cellphone', 'password']
     
     def clean_senha(self):
         senha = self.cleaned_data.get("senha")
