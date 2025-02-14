8980983f376ef849b17c18ec050859ca456813b4
CROMABLUE <jsmoura.dev@gmail.com>
2025-02-14 13:10:54 -0400

    :sparkles: feat: add UserForm

---
+++ ReservaUFAM/forms.py
@@ -0,0 +1,13 @@
+from .models import UserProfile
+from django import forms
+
+class UsuarioForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['SIAPE', 'CPF', 'nome', 'email', 'telefone', 'senha']
+    
+    def clean_senha(self):
+        senha = self.cleaned_data.get("senha")
+        if len(senha) < 8 or len(senha) > 12:
+            raise forms.ValidationError("A senha deve ter entre 8 e 12 caracteres.")
+        return senha
