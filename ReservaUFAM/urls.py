833cfe2fd615205bfe9832e73f53ad0a4d5d040b
CROMABLUE <jsmoura.dev@gmail.com>
2025-02-14 15:34:07 -0400

    :sparkles: feat: add news urls, login and logout

--- ReservaUFAM/urls.py
+++ ReservaUFAM/urls.py
@@ -2,8 +2,12 @@
 from django.urls import path
 from django.conf import settings
 from . import views
+from .views import login_user
+
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('register', views.register_user, name='register'),
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_user, name='logout'),
 ]
