4f5c1405a87e318b608a19d9c9b352541cba1a87
CROMABLUE <jsmoura.dev@gmail.com>
2025-02-17 16:19:57 -0400

    :sparkles: feat: add new table to the django admin panel, AllowedUser

--- ReservaUFAM/admin.py
+++ ReservaUFAM/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile, AdminProfile, Reserva
+from .models import UserProfile, AdminProfile, Reserva, AllowedUser
 
 admin.site.register(UserProfile)
 admin.site.register(AdminProfile)
 admin.site.register(Reserva)
+admin.site.register(AllowedUser)
