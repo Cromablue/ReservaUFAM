dc82432837d818a68b31ba6b66f9b82bf53c3409
CROMABLUE <jsmoura.dev@gmail.com>
2025-02-25 16:22:56 -0400

    :sparkles: feat: add the models to admin panel

--- backend/reserve/admin.py
+++ backend/reserve/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Auditorium, MeetingRoom, Vehicle, Reservation
 
-# Register your models here.
+admin.site.register(Auditorium)
+admin.site.register(MeetingRoom)
+admin.site.register(Vehicle)
+admin.site.register(Reservation)
