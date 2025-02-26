a2f5516665dcb5863c9385888ea5827391d0925a
CROMABLUE <jsmoura.dev@gmail.com>
2025-02-26 19:56:47 -0400

    :bug: fix: fix the use of CustomUser

--- backend/reserve/views.py
+++ backend/reserve/views.py
@@ -1,4 +1,3 @@
-# Importa as dependências necessárias para o funcionamento da API, incluindo autenticação, permissões e manipulação de erros.
 from django.contrib.auth import authenticate
 from django.core.exceptions import PermissionDenied
 from rest_framework import generics, permissions, status
@@ -6,127 +5,143 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.exceptions import ValidationError
-from django.contrib.auth.models import User
 from django.utils.timezone import now
-from .models import Reservation
-from .serializers import UserProfileSerializer, ReservationSerializer 
+from django.shortcuts import render, get_object_or_404
+from .models import Reservation, Auditorium, MeetingRoom, Vehicle
+from .serializers import (
+    AuditoriumSerializer, MeetingRoomSerializer, VehicleSerializer,
+    CustomUserSerializer, ReservationSerializer
+)
 
-# Cadastro de usuário: Classe responsável pela criação de novos usuários.
+def index(request):
+    return render(request, 'index.html')
+
+# Painel do administrador para gerenciar auditórios
+class AuditoriumAdminView(generics.ListCreateAPIView):
+    queryset = Auditorium.objects.all()
+    serializer_class = AuditoriumSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+class AuditoriumDetailAdminView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Auditorium.objects.all()
+    serializer_class = AuditoriumSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+# Painel do administrador para gerenciar salas de reunião
+class MeetingRoomAdminView(generics.ListCreateAPIView):
+    queryset = MeetingRoom.objects.all()
+    serializer_class = MeetingRoomSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+class MeetingRoomDetailAdminView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MeetingRoom.objects.all()
+    serializer_class = MeetingRoomSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+# Painel do administrador para gerenciar veículos
+class VehicleAdminView(generics.ListCreateAPIView):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+class VehicleDetailAdminView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+# Cadastro de usuário
 class RegisterView(APIView):
     def post(self, request):
-        # Usa o serializer para validar e salvar os dados do usuário.
-        serializer = UserProfileSerializer(data=request.data)
-        
-        # Verifica se os dados são válidos.
+        serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            password = serializer.validated_data['password']
-
-            # Valida a senha: ela precisa ter pelo menos 8 caracteres, conter letras e números.
-            if len(password) < 8 or not any(char.isdigit() for char in password) or not any(char.isalpha() for char in password):
-                raise ValidationError("A senha deve ter pelo menos 8 caracteres, incluindo letras e números.")
-
-            # Criptografa a senha e salva o usuário no banco.
-            user.set_password(password)  
+            password = request.data.get('password')
+            user.set_password(password)
             user.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-        # Retorna erros caso o serializer não seja válido.
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# Login de usuário: Classe responsável pela autenticação do usuário.
+# Login de usuário
 class LoginView(APIView):
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
-
-        # Verifica as credenciais e autentica o usuário.
         user = authenticate(username=username, password=password)
-
         if user and user.is_active:
-            # Gera tokens JWT para o usuário.
             refresh = RefreshToken.for_user(user)
             return Response({
                 "refresh": str(refresh),
                 "access": str(refresh.access_token)
             }, status=status.HTTP_200_OK)
-        
-        # Levanta erro caso as credenciais sejam inválidas ou o usuário esteja inativo.
         raise ValidationError("Credenciais inválidas ou usuário inativo.")
 
-# Solicitação de reserva: Classe para criação de reservas.
-class CreateReservationView(generics.CreateAPIView):
-    queryset = Reservation.objects.all()
-    serializer_class = ReservationSerializer
-    permission_classes = [permissions.IsAuthenticated]  # Exige que o usuário esteja autenticado
-
-    def perform_create(self, serializer):
-        # Atribui o usuário logado à reserva e define o status como 'Pendente'.
-        serializer.save(user=self.request.user, status='Pendente')
-
-# Lista de reservas para administradores: Classe para visualizar todas as reservas de administradores.
+# Listagem de reservas para administradores
 class AdminReservationListView(generics.ListAPIView):
     serializer_class = ReservationSerializer
-    permission_classes = [permissions.IsAdminUser]  # Exige que o usuário seja administrador
+    permission_classes = [permissions.IsAdminUser]
 
     def get_queryset(self):
-        # Obtém parâmetros de filtro (status e categoria) da query string da requisição.
         status_filter = self.request.query_params.get('status', 'Pendente')
         category_filter = self.request.query_params.get('category', '').lower()
-
-        # Filtra as reservas com base no status e na data final.
-        queryset = Reservation.objects.filter(status=status_filter, final_date__gte=now().date()).order_by('initial_date')
-
-        # Filtra por categoria se for 'auditorio' ou 'veiculo'.
+        queryset = Reservation.objects.filter(status=status_filter, final_date__gte=now().date(), is_deleted=False).order_by('initial_date')
         if category_filter == 'auditorio':
             queryset = queryset.filter(auditorium__isnull=False)
         elif category_filter == 'veiculo':
             queryset = queryset.filter(vehicle__isnull=False)
+        elif category_filter == 'sala de reunião':
+            queryset = queryset.filter(meeting_room__isnull=False)
+        return queryset.select_related('auditorium', 'vehicle', 'meeting_room')
 
-        # Usando `select_related` para otimizar a consulta e evitar consultas N+1.
-        return queryset.select_related('auditorium', 'vehicle')  
+# Listagem de reservas para usuário comum
+class UserReservationListView(generics.ListAPIView):
+    serializer_class = ReservationSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-# Aprovar/Reprovar reserva: Classe para administradores aprovar ou reprovar reservas.
+    def get_queryset(self):
+        return Reservation.objects.filter(
+            user=self.request.user,
+            final_date__gte=now().date(),
+            is_deleted=False
+        ).order_by('-initial_date')
+
+# Criação de reservas
+class CreateReservationView(generics.CreateAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        meeting_room = serializer.validated_data.get('meeting_room')
+        if meeting_room and not Reservation.objects.filter(
+            meeting_room=meeting_room,
+            initial_date=serializer.validated_data['initial_date'],
+            final_date=serializer.validated_data['final_date'],
+            initial_time__lt=serializer.validated_data['final_time'],
+            final_time__gt=serializer.validated_data['initial_time'],
+        ).exists():
+            serializer.save(user=self.request.user, status='Aprovado')
+        else:
+            serializer.save(user=self.request.user, status='Pendente')
+
+# Aprovar/Reprovar reserva
 class UpdateReservationStatusView(APIView):
-    permission_classes = [permissions.IsAdminUser]  # Exige que o usuário seja administrador
+    permission_classes = [permissions.IsAdminUser]
 
     def patch(self, request, pk):
-        # Obtém a reserva com o ID especificado.
         reservation = get_object_or_404(Reservation, pk=pk)
         status_choice = request.data.get('status')
-
-        # Verifica se o status enviado é válido.
         if status_choice not in ['Aprovado', 'Reprovado']:
             return Response({"error": "Status inválido."}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Atualiza o status da reserva e a salva.
         reservation.status = status_choice
         reservation.save()
         return Response({"message": f"Reserva {status_choice.lower()} com sucesso."}, status=status.HTTP_200_OK)
 
-# Lista de reservas do usuário logado: Classe para exibir as reservas de um usuário logado.
-class UserReservationListView(generics.ListAPIView):
-    serializer_class = ReservationSerializer
-    permission_classes = [permissions.IsAuthenticated]  # Exige que o usuário esteja autenticado
+# Cancelar reserva (marcar como deletada)
+class CancelReservationView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-    def get_queryset(self):
-        # Filtra as reservas do usuário logado, garantindo que a data final seja maior ou igual à data atual.
-        return Reservation.objects.filter(
-            user=self.request.user,
-            final_date__gte=now().date()
-        ).order_by('-initial_date')
-
-# Detalhes de uma reserva (admin e usuário): Classe para exibir detalhes de uma reserva específica.
-class ReservationDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Reservation.objects.all()
-    serializer_class = ReservationSerializer
-    permission_classes = [permissions.IsAuthenticated]  # Exige que o usuário esteja autenticado
-
-    def get_object(self):
-        # Obtém o objeto da reserva.
-        reservation = super().get_object()
-
-        # Verifica se o usuário tem permissão para acessar a reserva (se for o dono ou administrador).
-        if self.request.user == reservation.user or self.request.user.is_staff:
-            return reservation
-        raise PermissionDenied("Você não tem permissão para acessar esta reserva.")
+    def delete(self, request, pk):
+        reservation = get_object_or_404(Reservation, pk=pk, user=request.user, is_deleted=False)
+        reservation.is_deleted = True
+        reservation.save()
+        return Response({"message": "Reserva cancelada com sucesso."}, status=status.HTTP_200_OK)
