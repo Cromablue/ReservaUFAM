38a2fdf394fa84c429cc727992d4f079a17d5111
CROMABLUE <jsmoura.dev@gmail.com>
2025-02-26 19:13:24 -0400

    :sparkles: feat: add role to user model, now use AbstractUser

--- backend/reserve/models.py
+++ backend/reserve/models.py
@@ -1,20 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-# Model to store additional user information
-class UserProfile(models.Model):
-    # One-to-one relationship with Django's default User model
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+# Model for Users
+class CustomUser(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Administrator"
+        PROFESSOR = "PROFESSOR", "Professor"
+        TECHNICIAN = "TECHNICIAN", "Technician"
     
-    # Mandatory fields for user identification
-    siape = models.CharField(max_length=7, unique=True, null=False)  # Unique SIAPE code
-    cpf = models.CharField(max_length=11, unique=True, null=False)  # Unique CPF
-    name = models.CharField(max_length=255, null=False)  # Full name
-    email = models.EmailField(unique=True, blank=True, null=False)  # Unique email
-    cellphone = models.CharField(max_length=15, null=True, blank=True)  # Optional phone number
-    password = models.CharField(max_length=12, null=False)  # User password
-    
-    # User status in the system
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.TECHNICIAN)
+    siape = models.CharField(max_length=7, unique=True, null=False)
+    cpf = models.CharField(max_length=11, unique=True, null=False)
+    cellphone = models.CharField(max_length=15, null=True, blank=True)
     STATUS_CHOICES = [
         ('Pendente', 'Pending'),
         ('Aprovado', 'Approved'),
@@ -23,22 +20,8 @@ class UserProfile(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pendente', blank=True)
 
     def __str__(self):
-        return self.name  # Returns the user's name when printing the object
-    
-    # Model to store administrator information
-class AdminProfile(models.Model):
-    # One-to-one relationship with Django's default User model
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='admin_profile')
-    
-    # Administrator details
-    name = models.CharField(max_length=50)  # Full name
-    cellphone = models.CharField(max_length=11, unique=True)  # Unique phone number
-    cpf = models.CharField(max_length=11, unique=True)  # Unique CPF
-    siape = models.CharField(max_length=7, unique=True)  # Unique SIAPE
+        return f"{self.get_full_name()} ({self.get_role_display()})"
 
-    def __str__(self):
-        return self.name  # Returns the administrator's name when printing the object
-    
 # Model for Auditoriums
 class Auditorium(models.Model):
     name = models.CharField(max_length=100, unique=True)  # Unique auditorium name
