babe388015f1c1de4fccfe66283b9967a6415bef
CROMABLUE <jsmoura.dev@gmail.com>
2025-02-26 17:35:42 -0400

    :sparkles: feat: update the admin models access

--- backend/reserve/admin.py
+++ backend/reserve/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Auditorium, MeetingRoom, Vehicle, Reservation
+from .models import Auditorium, MeetingRoom, Vehicle, Reservation, UserProfile, AdminProfile
 
 admin.site.register(Auditorium)
 admin.site.register(MeetingRoom)
 admin.site.register(Vehicle)
 admin.site.register(Reservation)
+admin.site.register(UserProfile)
+admin.site.register(AdminProfile)
