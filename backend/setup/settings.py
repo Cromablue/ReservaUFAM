56370874e5f8c9fb6c0706dcb46d3bd46894c330
evelimbacury <evelimbacury@gmail.com>
2025-02-25 20:05:11 -0400

    :boom: fix: Fixing host and URL for Docker

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
