a9633a6c0d6f7c10d8300301a1948a44c12d0f44
CROMABLUE <jsmoura.dev@gmail.com>
2025-05-20 12:50:32 -0400

    :bug: Fix: add allowedHosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,7 +9,8 @@ export default defineConfig({
     },
     host: true,
     strictPort: true,
-    port: 5173
+    port: 5173,
+    allowedHosts: ['reservaufam.lab.local'] 
   },
   base: "/",
 })
