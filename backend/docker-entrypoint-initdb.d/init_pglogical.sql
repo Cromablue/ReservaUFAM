29cc822a07f41d2b079e00a909d8b2d44afff7df
CROMABLUE <jsmoura.dev@gmail.com>
2025-05-22 14:20:47 -0400

    :bug: Fix: custom pg to first server

--- backend/docker-entrypoint-initdb.d/init_pglogical.sql
+++ backend/docker-entrypoint-initdb.d/init_pglogical.sql
@@ -1,7 +1,7 @@
--- Cria a extensão pglogical (se ainda não existir)
+-- Ativar extensão
 CREATE EXTENSION IF NOT EXISTS pglogical;
 
--- Cria o usuário replicador se não existir
+-- Criação do replicator
 DO $$
 BEGIN
    IF NOT EXISTS (
@@ -12,27 +12,23 @@ BEGIN
 END
 $$;
 
--- Concede permissões ao usuário replicator (ajuste o nome do banco após a criação do banco)
--- OBS: este script será executado no contexto do banco definido como POSTGRES_DB
--- Portanto não é necessário mudar de banco aqui
-
-GRANT CONNECT ON DATABASE ${POSTGRES_DB} TO replicator;
+-- Permissões
+GRANT CONNECT ON DATABASE reserve_database TO replicator;
 GRANT USAGE ON SCHEMA public TO replicator;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO replicator;
 
--- Permite acesso a futuras tabelas automaticamente
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
   GRANT SELECT ON TABLES TO replicator;
 
--- Cria o node lógico
+-- Criar o node lógico (servidor primário)
 SELECT pglogical.create_node(
     node_name := 'servidor1_node',
-    dsn := 'host=reservaufam_db port=5432 dbname=your_database user=replicator password=replicator_password'
+    dsn := 'host=reservaufam_db port=5432 dbname=reserve_database user=replicator password=replicator_password'
 );
 
--- Cria a replication set com tabelas específicas (exemplo)
+-- Cria a replication set
 SELECT pglogical.create_replication_set('default', true, true, false, true);
 
--- Adiciona tabelas à replication set
+-- Adiciona tabelas específicas
 SELECT pglogical.replication_set_add_table('default', 'reserve_reservation', true);
 SELECT pglogical.replication_set_add_table('default', 'reserve_customuser', true);
